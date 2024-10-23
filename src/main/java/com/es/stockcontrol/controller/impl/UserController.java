e6ebca1302283db45ab280a95ae0f1d5062ef62d
Alfonso <anillo.roalf23@cadiz.salesianos.edu>
2024-10-23 09:17:22 +0200

    clase User

--- src/main/java/com/es/stockcontrol/controller/impl/UserController.java
+++ src/main/java/com/es/stockcontrol/controller/impl/UserController.java
@@ -4,12 +4,26 @@ package com.es.stockcontrol.controller.impl;
 import com.es.stockcontrol.controller.api.UserControllerAPI;
 import com.es.stockcontrol.model.RespuestaHTTP;
 import com.es.stockcontrol.model.User;
+import com.es.stockcontrol.service.UserService;
 
 public class UserController implements UserControllerAPI {
 
+    private UserService service;
+
+    public UserController(){
+        service = new UserService();
+    }
 
     @Override
     public RespuestaHTTP<User> login(String userInput, String passInput) {
-        return null;
+        try {
+            User u = service.login(userInput, passInput);
+
+            return u != null ?
+                    new RespuestaHTTP<>(200, "Usuario " + userInput + " logueado con éxito", u) :
+                    new RespuestaHTTP<>(400, "Usuario no encontrado");
+        } catch (Exception e){
+            return new RespuestaHTTP<>(500, "Fatal internal server");
+        }
     }
 }
--- src/main/java/com/es/stockcontrol/service/UserService.java
+++ src/main/java/com/es/stockcontrol/service/UserService.java
@@ -1,4 +1,44 @@
 package com.es.stockcontrol.service;
 
+import com.es.stockcontrol.model.User;
+import com.es.stockcontrol.repository.UserRepository;
+import com.es.stockcontrol.utils.Encrypt;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserService {
+
+    public UserRepository repository;
+
+    public UserService() {
+        repository = new UserRepository();
+    }
+
+    public User login(String nombre, String password) {
+        if(password == null || password.isEmpty()){
+            return null;
+        }
+
+        if(nombre == null || nombre.isEmpty()){
+            return null;
+        }
+
+        if(nombre.length() > 15) return null;
+
+        String regexNombre = "[a-zA-Z0-9]{1,15}$";
+        Pattern p = Pattern.compile(regexNombre, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(nombre);
+        if(!m.matches()) return null;
+
+        String passHash = Encrypt.encrypPassword(password);
+
+        return repository.login(nombre, password);
+    }
+
+    public List<User> getAllUsers() {
+        List<User> lista = repository.getAllUsers();
+        return lista;
+    }
 }
